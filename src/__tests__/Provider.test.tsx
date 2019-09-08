7a1b0e3d76efc46019ef7f2c9222cb73615fac4a
Christopher Tran <pfftdammitchris@gmail.com>
2019-09-07 22:56:38 -0700

    Prep tests

--- src/__tests__/Provider.test.tsx
+++ src/__tests__/Provider.test.tsx
@@ -1,20 +1,16 @@
 import * as React from 'react'
 
-it.todo('should render max 3 columns per row')
-it.todo('should render the header')
-it.todo('should render the snippet')
-it.todo('should render the action icons')
-it.todo('should be able to add more actions with props.actions')
-it.todo(
+it('should render max 3 columns per row')
+it('should render the header')
+it('should render the snippet')
+it('should render the action icons')
+it('should be able to add more actions with props.actions')
+it(
   'props.actions should be an array of objects with shape: { title, icon, ...rest }',
 )
-it.todo('columnCount should update when props.columnCount changes')
-it.todo('props.theme should set the initial theme if provided')
-it.todo('props.onThemeChange should invoke whenever state.theme updates if')
-it.todo('renderHeader should be able to overwrite the default header component')
-it.todo(
-  'renderSnippet should be able to overwrite the default snippet component',
-)
-it.todo(
-  'renderActions should be able to overwrite the default actions component',
-)
+it('columnCount should update when props.columnCount changes')
+it('props.theme should set the initial theme if provided')
+it('props.onThemeChange should invoke whenever state.theme updates if')
+it('renderHeader should be able to overwrite the default header component')
+it('renderSnippet should be able to overwrite the default snippet component')
+it('renderActions should be able to overwrite the default actions component')
--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -18,19 +18,20 @@ beforeEach(() => {
 
 describe('snippets', () => {
   describe('.formatSnippets', () => {
+    it('')
     // Jest puts each item in the 2nd level array as arguments to test func
     //    So we must wrap them with an additional array layer to fit our data structure
-    it.each(utils.formatSnippets(genSnippets(), 3).map((s) => [s]))(
-      `each item in snippets should be an array`,
-      (snippetsList) => {
-        expect(Array.isArray(snippetsList)).toBe(true)
-      },
-    )
-    it.each(utils.formatSnippets(genSnippets(), 4).map((s) => [s]))(
-      'each item in snippets should not contain more than the columnCount given',
-      (snippetsList) => {
-        expect(snippetsList.length).toBeLessThanOrEqual(4)
-      },
-    )
+    // it.each(utils.formatSnippets(genSnippets(), 3).map((s) => [s]))(
+    //   `each item in snippets should be an array`,
+    //   (snippetsList) => {
+    //     expect(Array.isArray(snippetsList)).toBe(true)
+    //   },
+    // )
+    // it.each(utils.formatSnippets(genSnippets(), 4).map((s) => [s]))(
+    //   'each item in snippets should not contain more than the columnCount given',
+    //   (snippetsList) => {
+    //     expect(snippetsList.length).toBeLessThanOrEqual(4)
+    //   },
+    // )
   })
 })
